135c8647b0a3f93c4e8803af89dd35b172e53584
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2019-04-24 19:56:16 +0000

    actualizacion de modelo
    relacion de uno a uno modelo localizaciones punto

--- app/Localizaciones.php
+++ app/Localizaciones.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Localizaciones extends Model
 {
-    //
+    /**
+    * Asignacion de atributos tabla
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'direccion','latitud_longitud','id_ciudad','id_empresa','id_persona'
+    ];
+
+    /**
+    * Relacion de puntos a ciudad uno a uno
+    */
+    public function ciudades()
+    {
+        return $this->belongsTo('App\Ciudades');
+    }
+    /**
+    * Relacion de puntos a empresas uno a uno
+    */
+    public function empresas()
+    {
+        return $this->belongsTo('App\Empresas');
+    }
 }
