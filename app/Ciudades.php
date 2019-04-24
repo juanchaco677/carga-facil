37d24037ea59d019f2979d75f450668aa7cc3be0
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2019-04-24 19:41:40 +0000

    actualizacion de modelo
    * Relacion de ciudades a departamentos muchos a uno
    * Relacion de ciudades a localizaciones uno a muchos

--- app/Ciudades.php
+++ app/Ciudades.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ciudades extends Model
 {
-    //
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'nombre'
+    ];
+
+    /**
+    * Relacion de ciudades a departamentos muchos a uno
+    */
+    public function departamentos()
+    {
+        return $this->belongsTo('App\Departamentos');
+    }
+    /**
+    * Relacion de ciudades a localizaciones uno a muchos
+    */
+    public function localizaciones()
+    {
+        return $this->hasMany('App\Localizaciones');
+    }
 }
