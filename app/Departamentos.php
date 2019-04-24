615ee11c97192ce02f5b4b2c60beb4781710cd5a
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2019-04-24 13:09:11 +0000

    Relacion mucho
    Se relaciona el pais con el departamento

--- app/Departamentos.php
+++ app/Departamentos.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Departamentos extends Model
 {
-    //
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'nombre'
+    ];
+
+    /**
+    * Relacion de pais a departamento uno a muchos
+    */
+    public function comments()
+    {
+        return $this->hasMany('App\Pais');
+    }
 }
