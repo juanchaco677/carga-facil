4d35a70b1a899222cb45445a79baa96d1d4dbae3
juanchaco677 <31424234+juanchaco677@users.noreply.github.com>
2019-04-24 13:27:12 +0000

    Relacion modelo
    relacion modelo  muchos a uno

--- app/Pais.php
+++ app/Pais.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pais extends Model
 {
-     /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
-     */
+    */
     protected $fillable = [
         'nombre'
     ];
+
+    /**
+    * Relacion de departamentos a pais muchos a uno
+    */
+    public function departamentos()
+    {
+        return $this->belongsTo('App\Departamentos');
+    }
 }
